4381472ce1d5b3fb060573dd74eb252189a74b61
Daniel Gartner <dsgartne@ncsu.edu>
2024-10-01 14:56:22 -0400

    Preliminary documentation

--- src-tauri/src/chrome_functions.rs
+++ src-tauri/src/chrome_functions.rs
@@ -34,13 +34,13 @@ fn get_version(connect_info: &mut ConnectInfo) -> Result<(), anyhow::Error> {
 fn chrome_binary_setup(connect_info: &ConnectInfo) -> Result<(), anyhow::Error> {
     // Install @puppeteer/browsers
     let pnpm_command = if cfg!(target_os = "windows") { "pnpm.cmd" } else { "pnpm" };
-    
     Command::new(pnpm_command)
         .args(&["add", "-g", "@puppeteer/browsers@2.4.0"])
         .output()?;
 
     println!("@puppeteer/browsers@2.4.0 installed successfully");
 
+    // Get directory to install chrome
     let mut chrome_path = std::env::current_dir().expect("Failed to get current directory");
     chrome_path.push("resources");
 
@@ -57,6 +57,7 @@ fn chrome_binary_setup(connect_info: &ConnectInfo) -> Result<(), anyhow::Error> 
         .output()?;
 
 
+    // Change permission if on a unix system (may have to revisit for windows)
     #[cfg(unix)] {
         let path_buf = get_chrome_binary_path(connect_info);
 
@@ -77,7 +78,7 @@ fn chrome_binary_setup(connect_info: &ConnectInfo) -> Result<(), anyhow::Error> 
     Ok(())
 }
 
-// Setup chromedriver
+// Setup chromedriver and chrome if needed
 pub fn chrome_setup(connect_info: &mut ConnectInfo) -> Result<(), anyhow::Error> {
     // Get chrome driver path (if exists)
     let chromedriver_path = get_chromedriver_path();
@@ -90,18 +91,21 @@ pub fn chrome_setup(connect_info: &mut ConnectInfo) -> Result<(), anyhow::Error>
         // Get latest chrome driver version
         get_version(connect_info)?;
 
+        // Make url to download chromedriver
         let download_url = format!("{}{}{}", connect_info.chromedriver_url.as_str(), connect_info.version, connect_info.os_url.as_str());
-
+        // Get data
         let response = get(download_url)?;
+        // Make a file to chromedriver zip
         let mut file = File::create(resources_path.join("chromedriver.zip"))?;
+        // Copy data from chromedriver as chromedriver zip
         copy(&mut response.bytes()?.as_ref(), &mut file)?;
 
         // Extract ChromeDriver
         let zip_file = File::open(resources_path.join("chromedriver.zip"))?;
         let mut archive = ZipArchive::new(BufReader::new(zip_file))?;
-
         archive.extract(resources_path)?;
 
+        // Since chromedriver had to be setup, also setup chrome
         chrome_binary_setup(connect_info)?;
 
         println!("Chromium setup!");
@@ -125,7 +129,7 @@ pub async fn start_chromedriver(connect_info: &ConnectInfo) -> Result<WebDriver,
 
     caps.set_binary(&path)?;
 
-    // Start ChromeDriver
+    // Start ChromeDriver exe
     let _chromedriver = Command::new(get_chromedriver_path())
     .arg("--port=9515")
     .arg("--verbose")
@@ -145,25 +149,28 @@ pub async fn start_chromedriver(connect_info: &ConnectInfo) -> Result<WebDriver,
         }
     }
 
+    // Make new webdrier session at localhost:9515
     let driver = WebDriver::new("http://localhost:9515", caps).await
         .map_err(|e| {
             eprintln!("WebDriver error: {:?}", e);
             e
     });
 
+    // Error catching
     match driver {
         Ok(_) => return Ok(driver.unwrap()),
         Err(err) => panic!("Error: {}", err),
     };
 }
 
+// Get the path of the chrome binary
 pub fn get_chrome_binary_path(connect_info: &ConnectInfo) -> PathBuf {
-    // Set the path to your custom Chrome binary
+    // Set the path to chrome folder
     let mut path_buf = std::env::current_dir().expect("Failed to get current directory");
     path_buf.push("resources");
     path_buf.push("chrome");
 
-    // Find chromium binary folder
+    // Find chromium binary folder inside of chrome folder for our version
     let paths = fs::read_dir(&path_buf).unwrap();
     let mut name = String::from("");
     for path in paths {
@@ -174,14 +181,14 @@ pub fn get_chrome_binary_path(connect_info: &ConnectInfo) -> PathBuf {
     }
     path_buf.push(&name);
 
-    // There should be only one folder in this folder, so get it
+    // There should be only one folder in this folder, so get it too
     let paths = fs::read_dir(&path_buf).unwrap();
     for path in paths {
         name = format!("{}", path.unwrap().path().display());
     }
     path_buf.push(&name);
 
-    // Cross-platform compatibility
+    // Cross-platform compatibility since different os will have different path endings
     match env::consts::OS {
         "macos" => {
             path_buf.push("Google Chrome for Testing.app");
